5880e26eee3c8ea3d9b538287bd6a8db36b5406a
Ariel Aschenmil <arielasche@gmail.com>
2023-08-22 11:23:59 -0300

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+
+  var obj = {
+    to: 'arielasche@gmail.com',
+    subjet: 'Contacto desde la web',
+    html: nombre + " se contacto por la web y quiere mas info a este correo : " + email + ". <br>" + ". <br> Su tel es : " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+
+module.exports = router;
